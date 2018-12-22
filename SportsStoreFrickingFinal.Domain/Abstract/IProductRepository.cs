857137a11fbac7fbe5d9d8a7004ede3eca60d6e0
Abhihshek Pal <palabhishek322@gmail.com>
2018-12-23 01:20:50 +0530

    Add project files.

---
+++ SportsStoreFrickingFinal.Domain/Abstract/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using SportsStoreFrickingFinal.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsStoreFrickingFinal.Domain.Abstract
+{
+    public interface IProductRepository
+    {
+        IQueryable<Product> Products { get; }
+    }
+}
---
+++ SportsStoreFrickingFinal/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using SportsStoreFrickingFinal.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStoreFrickingFinal.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository { get; set; }
+
+        public ProductController(IProductRepository productRepository)
+        {
+            this.repository = productRepository;
+        }
+
+        public ViewResult List()
+        {
+            return View(this.repository);
+        }
+    }
+}
