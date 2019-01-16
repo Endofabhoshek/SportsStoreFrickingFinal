9fb5e5a895c58d09e2e4ca079d8ce3414164cd6a
Abhihshek Pal <palabhishek322@gmail.com>
2019-01-16 18:56:15 +0530

    pg 263

---
+++ SportsStoreFrickingFinal/Controllers/AdminController.cs
@@ -0,0 +1,30 @@
+﻿using SportsStoreFrickingFinal.Domain.Abstract;
+using SportsStoreFrickingFinal.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStoreFrickingFinal.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductRepository productRepository;
+        public AdminController(IProductRepository repo)
+        {
+            this.productRepository = repo;
+        }
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View(productRepository.Products);
+        }
+
+        public ViewResult Edit(int productId)
+        {
+            Product product = productRepository.Products.Where(x => x.ProductId == productId).FirstOrDefault();
+            return View(product);
+        }
+    }
+}
